c212ec9ff840a636661706913d0f344e78108cd8
jsl <1765399481@qq.com>
2017-06-26 17:19:28 +0800

    添加倒计时

--- app/view/register/step2.js
+++ app/view/register/step2.js
@@ -3,12 +3,36 @@
  */
 import tpl from './tpl/step2.html'
 var step2 = Backbone.View.extend({
-    el: '.container',
-    initialize(){
-    },
-    render: function (query) {
-        this.$el.html(tpl);
-    },
+	el: '.container',
+	initialize() {},
+	events: {
+		'click .findPasswordCodeBtn': 'phoneCode'
+	},
+	phoneCode: function() {
+		var countdown = 60;
+		var ele=$(".findPasswordCodeBtn");
+		function settime() {
+			if(countdown == 0) {
+				ele.removeAttr("disabled");
+				ele.val("获取验证码");
+				countdown = 60;
+				clearTimeout(ele[0].settimes);
+				return false;
+			} else {
+				ele.attr("disabled", true);
+				ele.val("重新发送(" + countdown + ")");
+				countdown--;
+			}
+			ele[0].settimes = setTimeout(function() {
+				settime(ele)
+			}, 1000)
+		};
+		settime();
+		return false;
+	},
+	render: function(query) {
+		this.$el.html(tpl);
+	},
 });
 
 module.exports = step2;
