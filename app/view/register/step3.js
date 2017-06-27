760e27c950be433aca61c1887970b08e4476bfbb
jsl <1765399481@qq.com>
2017-06-27 16:35:48 +0800

    图片回显

--- app/view/register/step3.js
+++ app/view/register/step3.js
@@ -6,31 +6,64 @@ import { imgModalBig } from '../../publicFun/public'
 var step3 = Backbone.View.extend({
 	el: '.container',
 	initialize() {},
+	events: {
+		'change #file0': 'changeImg0',
+		'change #file1': 'changeImg1',
+		'change #file2': 'changeImg2',
+	},
 	render: function(query) {
 		this.$el.html(tpl);
 		imgModalBig('.businessLicense', { 'width': 500, 'src': '../../../../asset/img/lince.jpg' });
-		imgModalBig('.frontPhoto', { 'width': 500, 'src': '../../../../asset/img/ID-up.jpg' });
-		imgModalBig('.backPhoto', { 'width': 500, 'src': '../../../../asset/img/ID-down.jpg' });
+		imgModalBig('.frontPhoto', { 'width': 500, 'src': '../../../../asset/img/ID-front.png' });
+		imgModalBig('.backPhoto', { 'width': 500, 'src': '../../../../asset/img/ID-back.png' });
 	},
-	changeImg: function() {
-		alert(1)
+	changeImg0: function() {
 		var num = 0;
+		this.changeImg(num);
+	},
+	changeImg1: function() {
+		var num = 1;
+		this.changeImg(num);
+	},
+	changeImg2: function() {
+		var num = 2;
+		this.changeImg(num);
+	},
+	changeImg: function(num) {
 		var preview = document.getElementById('photo' + num);
 		var file = document.getElementById("file" + num);
-		if(typeof FileReader == 'undefined') {
-			//			_self.img[num] = file.value;
-			//			$(_eve.target).select();
-			//			$(_eve.target).blur()
-			var path = document.selection.createRange().text;
-			// preview.innerHTML = '<div class="img" style="width:127px;height: 87px;filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\'' + file.value + '\'"></div>';
-			document.getElementById('photo' + num).style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='scale',src=\"" + path + "\")";
-		} else {
-			var reader = new FileReader();
-			reader.readAsDataURL(file.files[0]);
-			reader.onload = function(e) {
-				preview.src = e.target.result;
+		//		if(typeof FileReader == 'undefined') {
+		//			//			_self.img[num] = file.value;
+		//			//			$(_eve.target).select();
+		//			//			$(_eve.target).blur()
+		//			var path = document.selection.createRange().text;
+		//			// preview.innerHTML = '<div class="img" style="width:127px;height: 87px;filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\'' + file.value + '\'"></div>';
+		//			document.getElementById('photo' + num).style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='scale',src=\"" + path + "\")";
+		//		} else {
+		var reader = new FileReader();
+		reader.readAsDataURL(file.files[0]);
+		reader.onload = function(e) {
+			var image = new Image();
+			var show1 = false;
+			image.onload = function() {
+				var height = image.height;
+				var width = image.width;
+				console.log(height, width)
+				if(height / width < 112 / 163) {
+					show1 = true;
+				}
+			};
+			image.src = this.result;
+			$("#file0").height(24);
+			$("#ajaxForm0 a").show()
+			$("#photo" + num).css("background", "url(" + this.result + ") no-repeat center center");
+			if(show1){
+				$("#photo" + num).css("background-size", "163px auto");
+			}else{
+				$("#photo" + num).css("background-size", "auto 112px");
 			}
 		}
+		//		}
 	}
 });
 
