b1fa7aa1f80bbb4e29ba53531fa5617f8f33776e
tanzhen <75056419@qq.com>
2017-06-30 17:46:35 +0800

    签章记录显示详情

--- app/view/index/index.js
+++ app/view/index/index.js
@@ -7,6 +7,24 @@ var index = Backbone.View.extend({
     initialize(){
         $(".wrapper").prepend(uhead)
     },
+    events: {
+        'click .jilulist ul li .file': 'Toggleshow',
+    },
+    Toggleshow(event) {
+        var _this = event.currentTarget
+        var ind = $(_this).parent(".jilulist ul li").index();
+        var int = $(_this).parent(".jilulist ul li")
+        $(".jilulist ul li .details").slideUp();
+        $(".jilulist ul li").removeClass();
+        var toggle = $(_this).parent(".jilulist ul li").find(".details");
+        if (toggle.is(":hidden")) {
+            toggle.slideDown();
+            $(int).addClass('active');	
+        } else {
+            toggle.slideUp();
+            $(int).removeClass('active');
+        };
+    },    
     render: function (query) {
         this.$el.html(tpl);
     },
