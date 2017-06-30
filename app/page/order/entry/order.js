b5de60aa08cb7cea27173daa7905685ee2960939
wangtao <wangtao@example.com>
2017-06-30 15:49:37 +0800

    head unify

--- app/page/order/entry/order.js
+++ app/page/order/entry/order.js
@@ -7,11 +7,11 @@ console.log(add())
 var Router = require('../Router');
 var Util = require('../../../publicFun/public');
 var tpl = require('../../../view/pub/tpl/main.html');
-
+var header=require('../../../view/pub/tpl/uhead.html')
 $('body').prepend(tpl({
 
 }));
-
+$(".wrapper").prepend(header)
 window.S = {};
 
 S.router = new Router();
