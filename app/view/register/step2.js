fc097a00ac86396d51cd6fc22f6e336cc9d032db
jsl <1765399481@qq.com>
2017-07-03 10:15:15 +0800

    获取手机验证码 校验手机

--- app/view/register/step2.js
+++ app/view/register/step2.js
@@ -9,33 +9,45 @@ var step2 = Backbone.View.extend({
 	},
 	events: {
 		'click .findPasswordCodeBtn': 'phoneCode',
-		'click #goStep3':'goStep3'
+		'click #goStep3': 'goStep3',
+		'keyup .countCode': 'checkCode',
 	},
 	phoneCode: function() {
-		var countdown = 60;
-		var ele=$(".findPasswordCodeBtn");
-		function settime() {
-			if(countdown == 0) {
-				ele.removeAttr("disabled");
-				ele.val("获取验证码");
-				countdown = 60;
-				clearTimeout(ele[0].settimes);
-				return false;
-			} else {
-				ele.attr("disabled", true);
-				ele.val("重新发送(" + countdown + ")");
-				countdown--;
-			}
-			ele[0].settimes = setTimeout(function() {
-				settime(ele)
-			}, 1000)
-		};
-		settime();
-		return false;
+		this.model.set({ "clickEle": $(event.target).data('id') })
+		this.model.isValid()
+//		if((/^1[34578]\d{9}$/.test($(".countPhone").val()))) {
+		if(!this.model.isValid()) {
+			var countdown = 60;
+			var ele = $(".findPasswordCodeBtn");
+
+			function settime() {
+				if(countdown == 0) {
+					ele.removeAttr("disabled");
+					ele.val("获取验证码");
+					countdown = 60;
+					clearTimeout(ele[0].settimes);
+					return false;
+				} else {
+					ele.attr("disabled", true);
+					ele.val("重新发送(" + countdown + ")");
+					countdown--;
+				}
+				ele[0].settimes = setTimeout(function() {
+					settime(ele)
+				}, 1000)
+			};
+			settime();
+			return false;
+		}
 	},
-	goStep3:function(event){
-		this.model.set({"clickEle":$(event.target).data('id')})
-        this.model.isValid()
+	goStep3: function(event) {
+		this.model.set({ "clickEle": $(event.target).data('id') })
+		this.model.isValid()
+	},
+	checkCode: function() {
+		if($('.countCode').val().length==6){
+			alert($('.countCode').val())
+		}
 	},
 	render: function(query) {
 		this.$el.html(tpl);
