1e87ce4730fde9438e6e9a7300dd6c18c37ddf77
tanzhen <75056419@qq.com>
2017-07-04 12:23:49 +0800

    日志记录

--- app/view/logs/logs.js
+++ app/view/logs/logs.js
@@ -3,6 +3,25 @@ var logs = Backbone.View.extend({
     el: '.contents',
     initialize(){
     },
+    events: {
+        'click .jilulist ul li .file': 'Toggleshow',
+    },
+    //签章记录弹出详细记录
+    Toggleshow(event) {
+        var _this = event.currentTarget
+        var ind = $(_this).parent(".jilulist ul li").index();
+        var int = $(_this).parent(".jilulist ul li")
+        $(".jilulist ul li .details").slideUp();
+        $(".jilulist ul li").removeClass();
+        var toggle = $(_this).parent(".jilulist ul li").find(".details");
+        if (toggle.is(":hidden")) {
+            toggle.slideDown();
+            $(int).addClass('active');	
+        } else {
+            toggle.slideUp();
+            $(int).removeClass('active');
+        };
+    },    
     render: function(query) {
         this.$el.html(tpl);
     },
