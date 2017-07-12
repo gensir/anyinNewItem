3af9cfbc8750ff71a914f1bffbd91b41228cf0cb
jsl <1765399481@qq.com>
2017-07-12 18:24:38 +0800

    证书 扫描件上传mock

--- app/view/admin/step3.js
+++ app/view/admin/step3.js
@@ -1,6 +1,6 @@
 import tpl from './tpl/step3.html'
 import { imgModalBig } from '../../publicFun/public'
-var picture = [0, 0];
+var pictureFlag;
 var flag = true;
 var step3 = Backbone.View.extend({
 	el: '.container',
@@ -12,7 +12,7 @@ var step3 = Backbone.View.extend({
 	},
 	render: function(query) {
 		this.$el.html(tpl);
-		picture = [0, 0];
+		pictureFlag = [0, 0];
 		flag = true;
 		document.body.scrollTop = document.documentElement.scrollTop = 0;
 		imgModalBig('.digitalExample', { 'width': 500, 'src': '../../../../asset/img/apply.jpg' });
@@ -37,51 +37,92 @@ var step3 = Backbone.View.extend({
 			imageType = file.files[0].type;
 			regImage = /image\/\w+/;
 			if(file.files[0].size > 2 * 1024 * 1024) {
-				bootbox.alert({
-					className: "uploadPhoto",
+				bootbox.dialog({
+					className: "realName",
+					title: "<div class='title'></div>",
 					message: "<div class='message'>图片大小不要超过2M</div>",
 				})
 				return false;
 			}
 		}
 		if(!regImage.test(imageType)) {
-			bootbox.alert({
-				className: "uploadPhoto",
+			bootbox.dialog({
+				className: "realName",
+				title: "<div class='title'></div>",
 				message: "<div class='message'>请上传图片格式的文件</div>",
 			})
 			return false;
 		}
-		if(typeof FileReader == 'undefined') {
-			$("#file" + num).height(24);
-			$(".reset" + num).show()
-			file.select();
-			file.blur();
-			var path = document.selection.createRange().text;
-			//			preview.innerHTML = '<div class="img" style="width:127px;height: 87px;filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\'' + file.value + '\'"></div>';
-			document.getElementById('photo' + num).style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='scale',src=\"" + path + "\")";
-			picture[num] = 1;
-		} else {
-			var reader = new FileReader();
-			reader.readAsDataURL(file.files[0]);
-			reader.onload = function(e) {
-				var image = new Image();
-				image.src = this.result;
-				picture[num] = this.result;
-				image.onload = function() {
-					var height = image.height;
-					var width = image.width;
-					if((height / width) > (112 / 163)) {
-						$("#photo" + num).css("background-size", "auto 112px");
-					} else {
-						$("#photo" + num).css("background-size", "163px auto");
+		var randomPercent = Math.floor(Math.random() * 19 + 80);
+        var percentVal = 0;
+		$("#ajaxForm" + num).ajaxSubmit({
+			url: '/api/mp/file',
+			type: "post",
+			dataType: "json",
+			ContentType: "multipart/form-data",
+			beforeSend: function() {
+				$(".formPub").remove();
+				$(event.target).parent().addClass("form");
+				var div = $("<div/>");
+				div.attr('class', 'formPub');
+				div.appendTo($(event.target).parent());
+				var progress = $("<div/>");
+				progress.attr('class', 'progress');
+				progress.appendTo($(".formPub"))
+			},
+			uploadProgress: function(event, position, total, percentComplete) {
+				if (percentComplete < randomPercent) {
+                    percentVal = percentComplete;
+                } else {
+                    percentVal = randomPercent;
+                }
+                $(".progress").css({ "width": percentVal + '%' });
+			},
+			success: function(data) {
+				if(data.code == 0) {
+					var data = data.data.fullUrl;
+					pictureFlag[num] = data;
+					$("#photo" + num).css("background", "url(" + data + ") no-repeat center");
+					var reader = new FileReader();
+					reader.readAsDataURL(file.files[0]);
+					reader.onload = function(e) {
+						var image = new Image();
+						image.src = this.result;
+						image.onload = function() {
+							var height = image.height;
+							var width = image.width;
+							if((height / width) > (112 / 163)) {
+								$("#photo" + num).css("background-size", "auto 112px");
+							} else {
+								$("#photo" + num).css("background-size", "163px auto");
+							}
+						};
+						$(".reset" + num).show();
+						$("#file" + num).height(24);
+						imgModalBig('#photo' + num, { 'width': 500, 'src': pictureFlag[num] });
 					}
-				};
-				$("#file" + num).height(24);
-				$(".reset" + num).show()
-				$("#photo" + num).css("background", "url(" + this.result + ") no-repeat center center");
-				imgModalBig('#photo' + num, { 'width': 500, 'src': picture[num] });
-			}
-		}
+				} else {
+					var dialog = bootbox.alert({
+						className: "uploadPhoto",
+						message: data.msg,
+					})
+					return;
+				}
+			},
+			error: function(data) {
+				var dialog = bootbox.alert({
+					className: "uploadPhoto",
+					message: (data.msg || "网络异常，请稍后再试"),
+				})
+				return;
+			},
+			complete: function() {
+				setTimeout(function () {
+                    $(event.target).parent().removeClass("form");
+                    $(".formPub").remove();
+                }, 100);
+			},
+		})
 	},
 	choice: function(event) {
 		flag = false;
@@ -93,21 +134,20 @@ var step3 = Backbone.View.extend({
 		$(".curr-choice").html($(ele).parent().find('.sealName').html());
 	},
 	goStep4: function() {
-		if(picture[0] == 0) {
+		if(pictureFlag[0] == 0) {
 			var dialog = bootbox.alert({
 				className: "uploadPhoto",
 				message: "请上传《数字证书及电子印章申请表及用户责任书》扫描件",
 			})
 			return;
 		};
-		if(picture[1] == 0) {
+		if(pictureFlag[1] == 0) {
 			var dialog = bootbox.alert({
 				className: "uploadPhoto",
 				message: "请上传《法人授权书》扫描件",
 			})
 			return;
 		};
-		console.log(flag);
 		if(flag) {
 			var dialog = bootbox.alert({
 				className: "uploadPhoto",
