b6f44309997f72a07802ccb0986ea47dd970b780
jsl <1765399481@qq.com>
2017-07-19 16:48:03 +0800

    订单页面添加分页功能

--- app/view/orders/list.js
+++ app/view/orders/list.js
@@ -1,5 +1,6 @@
 import tpl from './tpl/list.html'
 //var dialogs=$(dialog()).prop("outerHTML");
+import { GetQueryString } from '../../publicFun/public.js'
 var service=require('../../server/service').default;
 var list = Backbone.View.extend({
     el: '.container',
@@ -7,17 +8,12 @@ var list = Backbone.View.extend({
     },
     events: {
         'click .eseallist .list>.nav': 'toggleList',
+        'click .PreviousPage': 'PreviousPage',
+        'click .NextPage': 'NextPage',
+        'click nav li.index': 'currentPapge'
     },
     render: function (query) {
-        service.queryOrderList(1,10).done(res=>{
-			var tempObj;
-			if(res.code != 0){
-                tempObj = {}
-			}else {
-                tempObj = res.data.list;
-			}
-			this.$el.html(tpl({data:tempObj}));
-		})
+    	this.listPage();
     },
     toggleList(event) {
         var _this = event.currentTarget;
@@ -30,6 +26,81 @@ var list = Backbone.View.extend({
             toggle.slideUp();
         }
     },
+    
+    listPage(querydata, pageNum, pageSize) {	   	
+        pageNum = pageNum || 1;
+        pageSize = pageSize || 5;
+        service.queryOrderList(pageNum, pageSize).done(res => {
+            var tempObj;
+            if (res.code != 0) {
+                tempObj = {}
+            } else {
+                tempObj = res.data;
+            }
+            this.model.set("totalPages", res.data.totalPages)
+            this.model.get("tplhtml").data = tempObj;
+            this.$el.html(tpl(this.model.get("tplhtml")));
+            this.pagination(res.data.pageNum, res.data.totalPages)
+        })
+    },
+    // 点击上一页、下一页
+    pagediv(val, totalPages) {
+        if (val < 1) {
+            val = 1;
+            return;
+        }
+        if (val > totalPages) {
+            val = totalPages;
+            return;
+        }
+        if (val === this.current) {
+            return;
+        }
+        var _that = this;
+        
+        this.listPage()
+    },
+    //pagination
+    pagination: function (pageNumber, totalPages) {
+        $("#pageLimit li.index").remove();
+        var firstShowPage, maxShowPage = 5
+        if (pageNumber <= 3) {
+            firstShowPage = 1
+        } else {
+            firstShowPage = pageNumber - 2;
+        }
+        var lastShowPage = maxShowPage + firstShowPage - 1;
+        if (lastShowPage > totalPages) {
+            lastShowPage = totalPages;
+        }
+        this.model.get("tplhtml").count = [];
+        for (var i = firstShowPage; i <= lastShowPage; i++) {
+            var pageIndex = '<li class="index"><a>' + i + '</a></li>';
+            $(".appendPage").before(pageIndex)
+        };
+        if (!this.active) {
+            this.active = $("#pageLimit .index").eq(0)
+        } else {
+            if (isNaN(this.active.find('a').text())) {
+                this.active = $("#pageLimit .index").eq(0)
+            }
+            this.active = $("#pageLimit a:contains(" + this.active.find('a').text() + ")").parents("li");
+        }
+        this.active.addClass("active").siblings().removeClass("active")
+    },
+    currentPapge(e) {
+        this.active = $(e.currentTarget);
+        var pageNum = this.active.find("a").text()
+        this.pagediv(pageNum, this.model.get("totalPages"))
+    },
+    PreviousPage() {
+        this.active = "";
+        this.pagediv(1, this.model.get("totalPages"))
+    },
+    NextPage(e) {
+        this.active = $(e.currentTarget).prev();
+        this.pagediv(this.model.get("totalPages"), this.model.get("totalPages"))
+    }
 });
 
 module.exports = list;
