489aba26e09faa55bed3a081199fe332383cd9f0
jsl <1765399481@qq.com>
2017-07-19 09:37:30 +0800

    admin step4产品修改需求

--- app/view/admin/step1.js
+++ app/view/admin/step1.js
@@ -4,7 +4,7 @@ var step1 = Backbone.View.extend({
 	el: '.container',
 	initialize() {
 
-		this.render();
+//		this.render();
 	},
 	events: {
 		'click #goStep2': 'goStep2',
@@ -15,6 +15,42 @@ var step1 = Backbone.View.extend({
 		this.$el.html(tpl);
 		sealstyle = [];
 		document.body.scrollTop = document.documentElement.scrollTop = 0;
+		bootbox.dialog({
+			className: "errorTips",
+			title: "<div class='title'>新办电子印章提示</div>",
+			message: "<div class='message'>" +
+				"<div class='icon'><span></span></div>" +
+				"<div class='errorOrderTips'>" +
+				"<div class='errorOrderTitle'>您还有未完成的订单,是否继续完成该订单?</div>" +
+				"<div class='errorOrderSeal'>" +
+				"<span>电子行政章</span><span>电子行政章</span></div>" +
+				"<div class='errorOrderRed'>新建订单将覆盖原有的订单,您需要重新填写资料</div></div><div class='clear'></div></div>",
+			buttons: {
+				cancel: {
+					label: "返回",
+					className: "btn1"
+				},
+				confirm1: {
+					label: "继续该订单",
+					className: "btn2",
+					callback: function() {
+						$(this).find(".message").html(123);
+						console.log($(this).find(".message").html())
+						if(i = 5) {
+							return false;
+						}
+
+					}
+				},
+				confirm2: {
+					label: "新建订单",
+					className: "btn3",
+					callback: function() {
+						window.location.href = "admin.html#step1";
+					}
+				}
+			}
+		})
 	},
 	goStep2: function(event) {
 		var hh = $('input:radio:checked').val();
--- app/view/admin/step4.js
+++ app/view/admin/step4.js
@@ -36,42 +36,7 @@ var step4 = Backbone.View.extend({
 		}
 	},
 	gopay: function() {
-		bootbox.dialog({
-			className: "errorTips",
-			title: "<div class='title'>新办电子印章提示</div>",
-			message: "<div class='message'>" +
-				"<div class='icon'><span></span></div>" +
-				"<div class='errorOrderTips'>" +
-				"<div class='errorOrderTitle'>您还有未完成的订单,是否继续完成该订单?</div>" +
-				"<div class='errorOrderSeal'>" +
-				"<span>电子行政章</span><span>电子行政章</span></div>" +
-				"<div class='errorOrderRed'>新建订单将覆盖原有的订单,您需要重新填写资料</div></div><div class='clear'></div></div>",
-			buttons: {
-				cancel: {
-					label: "返回",
-					className: "btn1"
-				},
-				confirm1: {
-					label: "继续该订单",
-					className: "btn2",
-					callback: function() {
-						$(this).find(".message").html(123);
-						console.log($(this).find(".message").html())
-						if(i = 5) {
-							return false;
-						}
-
-					}
-				},
-				confirm2: {
-					label: "新建订单",
-					className: "btn3",
-					callback: function() {
-						window.location.href = "admin.html#step1";
-					}
-				}
-			}
-		})
+		alert("支付");
 	},
 });
 
