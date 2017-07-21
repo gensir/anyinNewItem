30b2ce3ec0714094ede084cdb19c5afefd5938c4
tanzhen <75056419@qq.com>
2017-07-21 11:16:01 +0800

    获取随机码

--- app/view/register/step1.js
+++ app/view/register/step1.js
@@ -5,11 +5,12 @@ var step1 = Backbone.View.extend({
     initialize() {
     },
     events: {
+        'click .hang input': 'codetype',
         'click #xieyi': 'rules',
         'click #reguser': 'reguser',
         'keyup #idcode': 'checkidCode',
         //'keyup #yzmcode': 'checkyzmCode',
-        'click #up_yzmcode': 'captcha',
+        'click #up_yzmcode,.codeimg': 'captcha',
     },
 
     rules: function () {
@@ -17,6 +18,16 @@ var step1 = Backbone.View.extend({
             $('#reguser').prop("disabled", false);
         } else {
             $('#reguser').prop("disabled", true);
+        }
+    },
+    codetype() {
+        var val = $('input:radio[name="idcode"]:checked').val();
+        if (val == 1) {
+            $("#codetype").text("营业执照号：");
+            $("#idcode").attr({ placeholder: "请输入营业执照号", maxlength: "15" });
+        } else if (val == 0) {
+            $("#codetype").text("统一社会信用代码：");
+            $("#idcode").attr({ placeholder: "请输入18位统一社会信用代码", maxlength: "18" });
         }
     },
     //检查信用代码是否注册
@@ -37,45 +48,34 @@ var step1 = Backbone.View.extend({
         }
     },
     //检查随机验证码
-    checkyzmCode: function (data) {
-        if ($('#yzmcode').val().length == 4) {
-            var data = {
-                "yzmcode": $('#yzmcode').val(),
-            }
-            service.checkyzmCode(data).done(function (data) {
-                if (data.code == 0) {
-                    $("#yzmcode-error").html(data.msg).css({ "color": "#08c34e" });
-                } else {
-                    $("#yzmcode-error").html(data.msg);
-                }
-            })
-        } else {
-            $('#yzmcode-error').html('').css({ "color": "#f00" });
-        }
-    },
-    // 获取随机验证码；
-    yzmcode() {
-        var captchaProperties = {
-            captchaHash: ''
-        };
-        service.yzmcode().done(function (data) {
-            // $('#codeimg').html('<img class="codeimg" src="data:image/png;base64,' + result.data.data.image + '" />');
-            // captchaProperties.captchaHash = result.data.data.captchaHash;
-            $('#codeimg').html('<img class="codeimg" src="' + data.data.fileId + '" />');
-        })
-    },
-    captcha() {
-        service.captcha().done(function (data) {}).complete(()=>{
-            $(".codeimg").attr("src","/mp/captcha.jpg");
-        })
+    // checkyzmCode: function (data) {
+    //     if ($('#yzmcode').val().length == 4) {
+    //         var data = {
+    //             "yzmcode": $('#yzmcode').val(),
+    //         }
+    //         service.checkyzmCode(data).done(function (data) {
+    //             if (data.code == 0) {
+    //                 $("#yzmcode-error").html(data.msg).css({ "color": "#08c34e" });
+    //             } else {
+    //                 $("#yzmcode-error").html(data.msg);
+    //             }
+    //         })
+    //     } else {
+    //         $('#yzmcode-error').html('').css({ "color": "#f00" });
+    //     }
+    // },
 
-    },    
+    // 获取随机验证码；
+    captcha() {
+        $(".codeimg").attr('src', '/mp/captcha.jpg?' + Math.random());
+    },
+
     reguser: function (event) {
-    	var IDCode = $("#idcode").val();
-		window.reqres.setHandler("IDCode", function() {
-			return IDCode;
-		});
-		
+        var IDCode = $("#idcode").val();
+        window.reqres.setHandler("IDCode", function () {
+            return IDCode;
+        });
+
         this.model.set({ "clickEle": $(event.target).data('id') });
         if (!this.model.isValid()) {
             window.open('#step2', '_self')
@@ -84,7 +84,6 @@ var step1 = Backbone.View.extend({
 
     render: function (query) {
         this.$el.html(tpl);
-        this.captcha();
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     },
 });
