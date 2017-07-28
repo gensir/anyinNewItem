f0a351b5c9775f346b0b76d3a9ef08427bc06baa
wangtao <wangtao@example.com>
2017-07-28 14:16:12 +0800

    local test

--- app/server/server-proxy.js
+++ app/server/server-proxy.js
@@ -1,38 +1,43 @@
 var proxy = {
-    anyinUrl:'http://183.62.140.54',
-    test:'http://192.168.4.95:8080',
+    anyinUrl: 'http://183.62.140.54',
+    test: 'http://192.168.4.95:8080',
     // anyinUrl:'http://192.168.1.159:9500'
 }
 var config = {
-	dev: {
-		historyApiFallback: true,
-		stats: { colors: true },
-		hot: true,
-		inline: true,
-		progress: true,
-		port: 10086,
-		//contentBase:"./app/page/index",
-		proxy: {            
-			'/mp/':{
-				target: proxy.anyinUrl, //pathRewrite: {'^/column' : '/column'},
+    dev: {
+        historyApiFallback: true,
+        stats: { colors: true },
+        hot: true,
+        inline: true,
+        progress: true,
+        port: 10086,
+        //contentBase:"./app/page/index",
+        proxy: {
+            '/mp/': {
+                target: proxy.anyinUrl, //pathRewrite: {'^/column' : '/column'},
                 secure: false,
-				changeOrigin: true
-			},
-			'/mps':{
-				target: proxy.anyinUrl, //pathRewrite: {'^/column' : '/column'},
-                secure: false,
-				changeOrigin: true
+                changeOrigin: true
             },
-			'/common/':{
-				target: proxy.test, //pathRewrite: {'^/column' : '/column'},
+            '/mps': {
+                target: proxy.anyinUrl, //pathRewrite: {'^/column' : '/column'},
                 secure: false,
-				changeOrigin: true
+                changeOrigin: true
+            },
+            '/common/': {
+                target: proxy.test, //pathRewrite: {'^/column' : '/column'},
+                secure: false,
+                changeOrigin: true
+            },
+            '/sys/': {
+                target: proxy.test, //pathRewrite: {'^/column' : '/column'},
+                secure: false,
+                changeOrigin: true
             },
         }
-	},
-	pro: {
-		port: 10088
-	}
+    },
+    pro: {
+        port: 10088
+    }
 
 }
 module.exports = config;
