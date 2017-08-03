39cefd03d4e0e6c2a7422214b2a726eac13ceffd
jsl <1765399481@qq.com>
2017-08-03 17:18:08 +0800

    注册跳转放开

--- app/page/register/Router.js
+++ app/page/register/Router.js
@@ -93,11 +93,11 @@ var Router = Backbone.Router.extend({
         },'step4')
     },
     hashChange:function(){
-    	regStep=localStorage.regStep;
-    	hash = window.location.hash;
-    	if(hash!=regStep){
-    		window.open("register.html#step1", '_self')
-    	}
+//  	regStep=localStorage.regStep;
+//  	hash = window.location.hash;
+//  	if(hash!=regStep){
+//  		window.open("register.html#step1", '_self')
+//  	}
     }
 });
 
