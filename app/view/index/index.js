255ca5677e39e478500f26aea4bec7a3998fa952
tanzhen <75056419@qq.com>
2017-08-03 17:06:45 +0800

    用户信息 实名审核

--- app/view/index/index.js
+++ app/view/index/index.js
@@ -3,21 +3,38 @@ var service = require('../../server/service').default;
 import dialog from '../pub/tpl/dialog.html';
 import ukeys from '../../publicFun/ukeys';
 var dialogs = $($(dialog()).prop("outerHTML"));
-var esealCode,enterpriseCode,PKCS7;
+var esealCode,enterpriseCode,PKCS7,status;
 var esealCode = localStorage.esealCode;
 //var enterpriseCode = localStorage.enterpriseCode;
 var PKCS7 = localStorage.dSignature;
 var index = Backbone.View.extend({
     el: '.container',
     initialize() {
-        //console.log(ukeys.ukeyName())
-        //alert(ukeys.PIN("123456",0))
-        //ukeys.dSignature()
-        //this.load();
-        //this.datecalc()
+    },
+    render: function () {
+        //this.$el.html(tpl);
+        this.userinfo();
+        this.getEsealList();
+        this.logslist();
     },
     events: {
         'click .jilulist ul li .file': 'Toggleshow',
+    },
+    userinfo: function (event) {
+        var _this = this
+        var userdata = {
+            "username": "深圳市创业印章科技有限公司",
+            "status": "0",
+            "loginDate": "2017-07-01"
+        }
+        this.model.get("tpl").userinfo = userdata;
+        this.$el.html(tpl(this.model.get("tpl")));
+        if (this.model.get("tpl").userinfo.status == 0) {
+            _this.realname_Unknown();
+        } else if (this.model.get("tpl").userinfo.status == 2) {
+            _this.realname_no();
+        }
+
     },
     //签章记录弹出详细记录
     Toggleshow(event) {
@@ -35,7 +52,7 @@ var index = Backbone.View.extend({
             $(int).removeClass('active');
         };
     },
-    load() {
+    realname_Unknown() {
         bootbox.dialog({
             backdrop: true,
             closeButton: false,
@@ -55,7 +72,26 @@ var index = Backbone.View.extend({
         })
         return false;
     },
-
+    realname_no() {
+        bootbox.dialog({
+            backdrop: true,
+            closeButton: false,
+            className: "common realname no",
+            title: dialogs.find(".realname.no .title")[0].outerHTML,
+            message: dialogs.find(".realname.no .msg1")[0].outerHTML,
+            buttons: {
+                cancel: {
+                    label: "返回",
+                    className: "btn1",
+                    callback: function (result) {
+                        //console.log(result, "cancel")
+                        result.cancelable = false;
+                    }
+                },
+            }
+        })
+        return false;
+    },
     //获取签章记录数据
     logslist(pageNum, pageSize, data) {
         pageNum = pageNum || 1;
@@ -72,7 +108,7 @@ var index = Backbone.View.extend({
             } else {
                 logsObj = res.data.list;
             }
-            this.model.get("tpl").esealdata = logsObj;
+            this.model.get("tpl").logdata = logsObj;
             this.$el.html(tpl(this.model.get("tpl")));
         });
     },
@@ -83,7 +119,6 @@ var index = Backbone.View.extend({
         var date = (date2.getTime() - date1.getTime()) / (24 * 60 * 60 * 1000);
         if (date < 0) {
             console.log("已过期")
-            //alert("已过期");
         } else {
             console.log(parseInt(date) + '天')
             //alert(parseInt(date) + '天');
@@ -94,7 +129,7 @@ var index = Backbone.View.extend({
         pageNum = pageNum || 1;
         pageSize = pageSize || 3;
         var data = {
-            "firmId": "nihao",
+            "firmId": enterpriseCode || "nihao",
         };
         service.getEsealList(pageNum, pageSize, data).done(res => {
             var Esealobj;
@@ -103,14 +138,9 @@ var index = Backbone.View.extend({
             } else {
                 Esealobj = res.data.list;
             }
-            this.model.get("tpl").listdata = Esealobj;
+            this.model.get("tpl").esealdata = Esealobj;
             this.$el.html(tpl(this.model.get("tpl")));
         });
-    },
-    render: function () {
-        //this.$el.html(tpl);
-        this.getEsealList();
-        this.logslist();
     },
 });
 
