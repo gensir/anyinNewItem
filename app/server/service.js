9bcc57460767448c4b015d510198641f6fd9a7fe
tanzhen <75056419@qq.com>
2017-08-08 17:25:39 +0800

    post

--- app/server/service.js
+++ app/server/service.js
@@ -229,7 +229,7 @@ export default {
     },
     //名称查询企业编码
     checkname(data) {
-        return this.ajaxCall({ url: domain + basemp + "check_organization/web/solr/company/name", data: data });
+        return this.ajaxCall({ url: domain + basemp + "check_organization/web/solr/company/name", data: data }, "POST");
     },
     //登录权限控制
     loginLicense(data) {
