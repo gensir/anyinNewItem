d8bfe081c223baad5490f28116c8ded0fdca48df
wangtao <wangtao@example.com>
2017-08-16 18:18:45 +0800

    密码校验修改

--- app/publicFun/validate.js
+++ app/publicFun/validate.js
@@ -10,6 +10,8 @@ var verify = {
                     var text = text || _this.log[log]
                     $(ele + "-error").html(text)
                     _this.synError(ele);
+                }else{
+                    $(ele + "-error").html("")
                 };
                 if ($(ele + "-error").html() && /#f00|rgb\(255, 0, 0\)|red/.test($(ele + "-error").css("color"))) {
                     _this.result = false;
@@ -47,7 +49,7 @@ var verify = {
     },
     log: {
         phone: '请输入正确的手机号码',
-        passwd: '请输入6-18位字母、数字、特殊符号',
+        passwd: '请输入8-20位字母、数字、特殊符号',
         valId: '法人身份证号不正确',
         space: '经办人姓名不能为空',
         idcode: '请输入18位社会信用代码',
@@ -64,7 +66,7 @@ var verify = {
             return reg.test($(ele).val());
         },
         passwd: function (ele) {
-            var reg = /^([^(\s|\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5)]|\w){8,}$/;
+            var reg = /^([^(\s|\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5)]|\w){8,20}$/;
             return reg.test($(ele).val())
         },
         valId: function (ele) {
