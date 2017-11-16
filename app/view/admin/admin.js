5326f82c37d20b10cae926decbfc8b7459febb5b
xiongxiangdong <276550263@qq.com>
2017-11-16 10:35:42 +0800

    admin 页面UI

--- app/view/admin/admin.js
+++ app/view/admin/admin.js
@@ -51,8 +51,14 @@ define([
             var toggle = $(_this).parent(".list").find(".toggle");
             if (toggle.is(":hidden")) {
                 toggle.slideDown();
+                $(_this).addClass("nav_tac");
+                $(_this).next(".showHide").find(".tog_bot").click(function() {
+                    toggle.slideUp();
+                    $(_this).removeClass("nav_tac");
+                });                
             } else {
                 toggle.slideUp();
+                $(_this).removeClass("nav_tac");
             }
         },
         toggleTab: function(event, license) {
