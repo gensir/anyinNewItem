da516b19b6cd0de83639107c7bf0fe4e8ae0c0a0
jsl <1765399481@qq.com>
2017-11-20 10:04:42 +0800

    firmId 之前是根据公司名查   后来改成登录带回

--- app/view/admin/step2.js
+++ app/view/admin/step2.js
@@ -23,11 +23,7 @@ var step2 = Backbone.View.extend({
 		var isODC = $.cookie('loginadmin') && JSON.parse($.cookie('loginadmin')).loginType;
         //2为ODC
         //如果是ODC登录
-        if(isODC==2){			
-        	firmId = localStorage.indexFirmid;
-        }else{
-        	this.firmId = $.cookie('loginadmin') && JSON.parse($.cookie('loginadmin')).user.firmId
-        }
+        firmId = $.cookie('loginadmin') && JSON.parse($.cookie('loginadmin')).user.firmId;
 		this.$el.html(tpl);
 		document.body.scrollTop = document.documentElement.scrollTop = 0;
 		imgModalBig('.shadow1', { 'width': 500, 'src': '../../../../asset/img/lince.jpg' });
