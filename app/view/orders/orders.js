c42f40fbaba2dc2a67fdfe48226172fa77e7166a
tanzhen <75056419@qq.com>
2017-12-20 14:20:59 +0800

    orders

--- app/view/orders/orders.js
+++ app/view/orders/orders.js
@@ -11,7 +11,7 @@ define([
     var dialogs = $(dialogs);
     var that;
     var enterpriseCode = $.cookie('loginadmin') && JSON.parse($.cookie('loginadmin')).user.enterpriseCode
-    var firmId=$.cookie('loginadmin') && JSON.parse($.cookie('loginadmin')).user.firmId;
+    var firmId = $.cookie('loginadmin') && JSON.parse($.cookie('loginadmin')).user.firmId;
 
     var main = Backbone.View.extend({
         el: '.contents',
@@ -24,17 +24,18 @@ define([
             'click #continue': 'continueGo',
             'click #update_key': 'update_key',
             'click #renew': 'renew'
-        }, //this.$el.empty().html(template.compile(orders)({}));    
+        },
+        //this.$el.empty().html(template.compile(orders)({}));    
         render: function(query) {
             that = this;
-            this.listPage();
+            that.listPage();
         },
-        
-        continueGo: function(event) {       	
+
+        continueGo: function(event) {
             event.stopPropagation();
             var orderNo = $(event.currentTarget).parent().siblings(".nav0").text();
             service.status(orderNo).done(function(data) {
-                if(data.code == 0) {
+                if (data.code == 0) {
                     localStorage.orderNo = data.data.orderNo;
                     localStorage.stepNum = "#step" + data.data.operateStep;
                     window.location.href = "admin.html#step" + data.data.operateStep;
@@ -42,13 +43,13 @@ define([
             })
         },
         //续费未完成再支付
-        renew: function (event) {
+        renew: function(event) {
             event.stopPropagation();
             var keyType = $(event.currentTarget).data('type');
             var certificateFirm = $(event.currentTarget).data('cert');
             localStorage.keyType = $(event.currentTarget).data('type');
             localStorage.certificateFirm = $(event.currentTarget).data('cert');
-            if ((!keyType && !certificateFirm) || (certificateFirm == 2 )) {
+            if ((!keyType && !certificateFirm) || (certificateFirm == 2)) {
                 bootbox.dialog({
                     backdrop: true,
                     closeButton: false,
@@ -59,7 +60,7 @@ define([
                         confirm: {
                             label: "确定",
                             className: "btn2",
-                            callback: function (result) {
+                            callback: function(result) {
                                 result.cancelable = false;
                             }
                         },
@@ -77,7 +78,7 @@ define([
                         confirm: {
                             label: "确定",
                             className: "btn2",
-                            callback: function (result) {
+                            callback: function(result) {
                                 result.cancelable = false;
                             }
                         },
@@ -130,16 +131,13 @@ define([
                 })
                 return false;
             }
-            //  else {
-            //     window.location.href = "admin.html#update_key?orderNo=?" + orderNo + "&esealCode=" + esealCode + "&oid=" + oid;
-            // }
         },
         toggleList: function(event) {
             var _this = event.currentTarget;
             var ind = $(_this).parent(".list").index();
             $(".eseallist .list .showHide").slideUp();
             var toggle = $(_this).parent(".list").find(".showHide");
-            if(toggle.is(":hidden")) {
+            if (toggle.is(":hidden")) {
                 toggle.slideDown();
                 $(".nav").removeClass("nav_tac");
                 $(_this).addClass("nav_tac");
@@ -159,27 +157,26 @@ define([
             pageNum = pageNum || 1;
             pageSize = pageSize || 10;
             var data = {
-                "enterpriseCode": enterpriseCode || "914403001923081808"
+                "enterpriseCode": enterpriseCode
             }
             service.queryOrderList(pageNum, pageSize, data).done(function(res) {
                 var tempObj;
-                if(res.code != 0) {
+                if (res.code != 0) {
                     tempObj = {}
                 } else {
                     tempObj = res.data;
-                    that.model.set("totalPages", res.data.totalPages)
-                    that.model.get("tplhtml").data = tempObj;
-                    //tat.$el.html(tpl(this.model.get("tplhtml")));
-                    that.$el.html(template.compile(orderstpl)(that.model.get("tplhtml")));
+                    _this.model.set("totalPages", res.data.totalPages)
+                    _this.model.get("tplhtml").data = tempObj;
+                    _this.$el.html(template.compile(orderstpl)(_this.model.get("tplhtml")));
+                    _this.pagination(pageNum, res.data.totalPages)
 
-                    that.pagination(pageNum, res.data.totalPages)
-                    if(res.data.list.length == 0) {
+                    if (res.data.list.length == 0) {
                         $(".listResult").show();
                         $("nav").hide();
                     }
-                    if(pageNum == 1) {
+                    if (pageNum == 1) {
                         $("li.PreviousPage").addClass("no");
-                    } else if(pageNum == res.data.totalPages) {
+                    } else if (pageNum == res.data.totalPages) {
                         $("li.NextPage").addClass("no");
                     } else {
                         $("li.PreviousPage,li.NextPage").removeClass("no");
@@ -189,15 +186,15 @@ define([
         },
         // 点击上一页、下一页
         pagediv: function(val, totalPages) {
-            if(val < 1) {
+            if (val < 1) {
                 val = 1;
                 return;
             }
-            if(val > totalPages) {
+            if (val > totalPages) {
                 val = totalPages;
                 return;
             }
-            if(val === this.current) {
+            if (val === this.current) {
                 return;
             }
             var data = {
@@ -209,30 +206,30 @@ define([
         pagination: function(pageNumber, totalPages) {
             $("#pageLimit li.index").remove();
             var firstShowPage, maxShowPage = 5
-            if(pageNumber <= 3) {
+            if (pageNumber <= 3) {
                 firstShowPage = 1
             } else {
                 firstShowPage = pageNumber - 2;
             }
             var lastShowPage = maxShowPage + firstShowPage - 1;
-            if(lastShowPage > totalPages) {
+            if (lastShowPage > totalPages) {
                 lastShowPage = totalPages;
             }
             this.model.get("tplhtml").count = [];
-            for(var i = firstShowPage; i <= lastShowPage; i++) {
+            for (var i = firstShowPage; i <= lastShowPage; i++) {
                 var pageIndex = '<li class="index"><a>' + i + '</a></li>';
                 $(".appendPage").before(pageIndex)
             };
-            if(!this.active) {
+            if (!this.active) {
                 this.active = $("#pageLimit .index").eq(0)
             } else {
-                if(this.active.hasClass("NextPage")) {
+                if (this.active.hasClass("NextPage")) {
                     this.active = $(".NextPage");
                 }
-                if(isNaN(this.active.find('a').text()) && this.active.prev().text() != this.model.get("totalPages")) {
+                if (isNaN(this.active.find('a').text()) && this.active.prev().text() != this.model.get("totalPages")) {
                     this.active = $("#pageLimit .index").eq(0)
                 }
-                if(this.active.prev().text() == this.model.get("totalPages")) {
+                if (this.active.prev().text() == this.model.get("totalPages")) {
                     this.active = this.active.prev()
                 }
                 this.active = $("#pageLimit a:contains(" + this.active.find('a').text() + ")").parents("li");
@@ -250,7 +247,7 @@ define([
         },
         NextPage: function(e) {
             this.active = $(".NextPage");
-            console.log(this.active.text(), this.model.get("totalPages"))
+            // console.log(this.active.text(), this.model.get("totalPages"))
             this.pagediv(this.model.get("totalPages"))
         }
 
